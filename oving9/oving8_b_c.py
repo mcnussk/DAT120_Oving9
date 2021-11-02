d2a25a3f66b1c1e422b14c870581b31d443fe936
Magnus King <magnus.king@lyse.net>
2021-11-02 09:28:02 +0100

    ferdiggjøring av oving 9

--- oving9/oving8_b_c.py
+++ oving9/oving8_b_c.py
@@ -16,59 +16,85 @@ class Flervalg:
 
 #metode __str__
     def __str__(self):
+        teller = 0
         sporsmaal = ""
         sporsmaal += self.spmtekst + '\n'
         sporsmaal += "alternativer: \n"
         for alternativ in self.alternativer:
-            sporsmaal += alternativ +"\n"
+            sporsmaal +=  str(teller) + ": " + alternativ +"\n" 
+            teller += 1
         return sporsmaal
         
         
 #metode sjekk svar, tar imot heltall som representerer svar
     def sjekk_svar(self):
-        brukersvar = int(input("Skriv inn svaret som et heltall 1-3: "))
+        brukersvar = input("Skriv inn svaret som et heltall 0-3: ")
         if brukersvar == self.svar:
-            print("\nRiktig svar!\n")
+            teller = 1
+            return teller
         else:
-            print("\nDet var visst feil.")
-            print("Det riktige svaret var alternativ " + str(self.svar) + "\n")
-        
+            teller = 0
+        return teller
+  
 #metode korrekt svar tekst, skal returnere teksten for riktig svar
     def korrekt_svar_tekst(self):
-         return self.svar
-         
+         print("Korrekt svar: " + self.alternativer[int(self.svar)] + "\n")
          
 if __name__ == "__main__":
-    s1 = "Hvor høyt er Eiffeltårnet?"
-    s1list = ["(1) 324 meter", "(2) 354 meter", "(3) 374 meter"]
-    s1riktig = 1
+   
     
-    s2 = "Hvor mange bein har en sommerfugl?"
-    s2list = ["(1) 4", "(2) 8", "(3) 6"]
-    s2riktig = 3
-    
-    spill = Flervalg(s1, s1list, s1riktig)
-    spill2 =Flervalg(s2, s2list, s2riktig)
-    print(spill)
-    spill.sjekk_svar()
-    print(spill2)
-    spill2.sjekk_svar()
-    
-    sporsmaal = []
-    fil_sporsmaal = []
-    fil_korrekt_svar = []
-    fil_alternativer = []
-    teller = 0
-    tekstfil = open("sporsmaalsfil.txt", "r", encoding = "UTF8")
-    for linje in tekstfil:
-        rad = linje.split(":")
-        fil_sporsmaal.append(rad[0])
-        fil_korrekt_svar.append(rad[1])
-        fil_alternativer.append(rad[2])
-        print(fil_sporsmaal[teller])
-        print(fil_korrekt_svar[teller])
-        print(fil_alternativer[teller])
-        teller = teller + 1
-    
-    
-    
+   
+   def linjeleser():
+        tekstfil = open("sporsmaalsfil.txt", "r", encoding = "UTF8")
+        fil_sporsmaal = []
+        fil_korrekt_svar = []
+        fil_alternativer = []
+        liste_spm = []
+        for linje in tekstfil:
+            linje = linje.strip()
+            rad = linje.split(":")
+            fil_sporsmaal.append(rad[0])
+            fil_korrekt_svar.append(rad[1])
+            fil_alternativer.append(rad[2].split(","))
+
+            rad[2] = rad[2].strip().strip("[]")
+            rad[2] = rad[2].strip(",")
+
+            alt_liste = rad[2].split(",")
+            alt_liste = [x.strip (" ") for x in alt_liste]  #fjerner mellomrommet før alternativene  
+            liste_spm.append(Flervalg(rad[0], alt_liste, rad[1].strip()))
+        return liste_spm
+            
+   sporsmol = linjeleser()
+   
+   poengsum1 = 0
+   poengsum2 = 0
+   for element in sporsmol:
+       print(str(element))
+       print("Spiller 1 sin tur!")
+       value = element.sjekk_svar()
+       if value == 1:
+           korrekt1 = True
+       else:
+           korrekt1 = False
+       poengsum1 += value
+       print("Spiller 2 sin tur!")
+       value = element.sjekk_svar()
+       if value == 1:
+           korrekt2 = True
+       elif value == 0:
+           korrekt2 = False
+       poengsum2 += value
+       
+       
+       element.korrekt_svar_tekst()
+       if korrekt1 == True:
+           print("Spiller 1: Korrekt")
+       elif korrekt1 == False:
+           print("Spiller 1: Feil")
+       if korrekt2 == True:
+           print("Spiller 2: Korrekt")
+       elif korrekt2 == False:
+           print("Spiller 2: Feil")
+   print("\nSpiller 1 sin poengsum er " + str(poengsum1))
+   print("Spiller 2 sin poengsum er " + str(poengsum2))
--- oving9/underveisfil.py
+++ oving9/underveisfil.py
@@ -7,7 +7,7 @@ Created on Fri Oct 29 10:35:45 2021
 # splitte ved :, ta tallverdi inn som svar, splitte ved :, 
 #append hver verdi inn i en liste som spørsmål
 
-sporsmaal = []
+"""sporsmaal = []
 fil_sporsmaal = []
 fil_korrekt_svar = []
 fil_alternativer = []
@@ -21,7 +21,34 @@ for linje in tekstfil:
     print(fil_sporsmaal[teller])
     print(fil_korrekt_svar[teller])
     print(fil_alternativer[teller])
-    teller = teller + 1
+    teller = teller + 1"""
+    l = [i.strip('[]') for i in l]
     
+  hello = [x.strip(' ') for x in hello]
 
+"""testis = " 2 "
+if testis == input("kjørdåå"):
+    print("ez")
+else:
+    print(":(")
+    
+    for alt in fil_alternativer[teller]:
+                alt_teller = 0
+                print(str(alt_teller) + ": " + fil_alternativer[teller])
+                alt_teller += 1"""
+    
+""" s1 = "Hvor høyt er Eiffeltårnet?"
+    s1list = ["(1) 324 meter", "(2) 354 meter", "(3) 374 meter"]
+    s1riktig = 1
+    
+    s2 = "Hvor mange bein har en sommerfugl?"
+    s2list = ["(1) 4", "(2) 8", "(3) 6"]
+    s2riktig = 3
+    
+    spill = Flervalg(s1, s1list, s1riktig)
+    spill2 =Flervalg(s2, s2list, s2riktig)
+    print(spill)
+    spill.sjekk_svar()
+    print(spill2)
+    spill2.sjekk_svar()"""
     
